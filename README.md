3ad4b8ef1e56829080758c41f21c97946fd2b611
Henrique Rodrigues <henriquerodrigues@MacBook-Pro-de-Henrique.local>
2015-11-25 02:37:27 -0200

    Site Henrique Rodrigues

---
+++ README.md
@@ -0,0 +1,12 @@
+# Meu site 
+
+Acesse meu site: [hjdesigner.github.io](https://hjdesigner.github.io) :metal:
+
+Você também pode me encontrar em:
+* [Twitter](https://twitter.com/coisadev)
+* [Facebook](https://www.facebook.com/hjFrontEnd)
+* [GitHub](https://github.com/hjdesigner)
+* [LinkedIn](http://www.linkedin.com/in/coisadedev)
+* [Youtube](https://www.youtube.com/channel/UC4W-b9Q1I0mzGGzFKOu-66g)
+* Email: hjdesigner23@gmail.com 
+
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,27 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+
+	$('.parallax').parallax();
+
+
+});
+
+$(function() {
+    // Baixar URLs do feed do canal
+    $.get('http://gdata.youtube.com/feeds/api/users/cadeachave/uploads?max-results=1&alt=json', function(result) {
+
+        var entries = result.feed.entry;
+
+        for(var i=0; i<entries.length; i++) {
+            var url = entries[i].link[0].href;   
+
+            // Transformar URL de vídeo em URL de embed
+            // ANTES: http://www.youtube.com/watch?v=aDqExWjK49Y&algumacoisaaqui
+            // DEPOIS: http://www.youtube.com/embed/aDqExWjK49Y
+            url = url.replace('/watch?v=', '/embed/');
+            url = url.substring(0, url.indexOf('&'));
+
+            // Fazer append dos vídeos
+            $('.player').append('<iframe width="480" height="270" src="' + url + '" frameborder="0" allowfullscreen></iframe>');
+        }
+    });
+});
