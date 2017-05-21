c6a41d6a8233fdfbc3b3056dbfce5fe775953bb2
Henrique Rodrigues <hjdesigner23@gmail.com>
2017-05-21 16:02:26 -0300

    Version beta

--- css-padawan/js/script.js
+++ css-padawan/js/script.js
@@ -88,6 +88,7 @@ $(function(){
     }
 
     function send(){
+
         var text = $speechInput.val();
         $.ajax({
             type: "POST",
@@ -102,11 +103,11 @@ $(function(){
             success: function(data){
                 prepareResponse(data);
             },
+            complete:function(data){
+                $('#carregando').hide();
+            },
             error: function(){
                 respond(messageInternalError);
-            },
-            complete: function(){
-                $('#carregando').hide();
             }
         })
         .always(function(){
