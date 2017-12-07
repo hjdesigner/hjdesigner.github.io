c3674462baa846987d4b8c516d2d3a735928f610
Henrique Melanda <hjdesigner23@gmail.com>
2017-12-07 18:30:32 -0200

    Teste carousel vanilha js

--- teste-carousel/js/scripts.js
+++ teste-carousel/js/scripts.js
@@ -1,2 +1,2 @@
-"use strict";document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector(".js_variablewlidth");lory(e,{rewind:!0,enableMouseEvents:!0,slidesToScroll:4})});
+"use strict";document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector(".js_variablewlidth");lory(e,{rewind:!0,enableMouseEvents:!0})});
 //# sourceMappingURL=scripts.js.map
